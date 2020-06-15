442433e67f8a4067409ba823b49af74389b6a311
yrr0r <yrr0r.g@gmail.com>
2020-06-15 10:31:11 +0800

    night vision

--- static/cssjs/utility.js
+++ static/cssjs/utility.js
@@ -1,3 +1,20 @@
+// Themes
+function changeTheme(name){
+    if(name == 'night'){
+        document.getElementById("mainview").classList.add("bg-dark", "text-white");
+        document.getElementById("body").style.backgroundColor = "black";
+        document.getElementById("footer").classList.add("text-white");
+    }
+    if(name == 'day'){
+        document.getElementById("mainview").classList.remove("bg-dark", "text-white");
+        document.getElementById("body").style.backgroundColor = "white";
+        document.getElementById("footer").classList.remove("text-white");
+    }
+}
 
-pageInfo = document.getElementById("pageInfo").innerText
-console.log(pageInfo)
+// The Bookmark Thing.
+if(document.getElementById("pageInfo") != null){
+    pageInfo = document.getElementById("pageInfo").innerText;
+    pageInfo = JSON.parse(pageInfo);
+    console.log(pageInfo) //LOG
+}
