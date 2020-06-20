805754b25e9da0a84a8e182dc32d884ba847afec
yrr0r <yrr0r.g@gmail.com>
2020-06-21 00:10:50 +0800

    Bookmarks!

--- static/cssjs/utility.js
+++ static/cssjs/utility.js
@@ -50,6 +50,43 @@ function flushbm() {
 // add a bookmark
 function addBookmark() {
     bm = { 'title': pageInfo.title, 'link': pageInfo.link, 'inSection': pageInfo.currentSection };
-    bookmark[pageInfo.title] = bm;
+    // Detect duplicate keys:
+    for(keys in bookmark){
+        if(bookmark[keys].title == pageInfo.title) return;
+    }
+    bookmark[(new Date().getTime())] = bm;
     flushbm();
+}
+// delete a bookmark
+function removebm(key){
+    delete bookmark[key];
+    flushbm();
+    populateBmSection();
+}
+
+// toggle the menu display
+function toggleBmDisplay(){
+    widget = document.getElementById("bmcard");
+    if(widget.hidden == true){
+        widget.hidden = false;
+    } else {
+        widget.hidden = true;
+    }
+    populateBmSection()
+}
+
+function populateBmSection(){
+    var widget = document.getElementById("bookmarksSection");
+    var injhtml = "<table class='table'>";
+    for(key in bookmark){
+        injhtml = injhtml + 
+        `<tr>
+        <td> <a href="${bookmark[key].link}">${bookmark[key].title}</a> </td>
+        <td> <button class="btn" onclick="removebm(${key})"> 
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M2.75 2.5h10.5a.25.25 0 01.25.25v10.5a.25.25 0 01-.25.25H2.75a.25.25 0 01-.25-.25V2.75a.25.25 0 01.25-.25zM13.25 1H2.75A1.75 1.75 0 001 2.75v10.5c0 .966.784 1.75 1.75 1.75h10.5A1.75 1.75 0 0015 13.25V2.75A1.75 1.75 0 0013.25 1zm-2 7.75a.75.75 0 000-1.5h-6.5a.75.75 0 000 1.5h6.5z"></path></svg>
+        </button> </td>
+        </tr>`;
+    }
+    injhtml += "</table>"
+    widget.innerHTML = injhtml;
 }
